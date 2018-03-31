74287f1a12b8c3645e36c5f7f3adfc417ba50e55
Stefan Quach <stefan.quach@wustl.edu>
2018-03-31 13:53:40 -0500

    Made AccelListner class to read accelerometer data

---
+++ SafeEzersize/app/src/main/java/com/example/stefan/safeezersize/AccelListener.java
@@ -0,0 +1,7 @@
+package com.example.stefan.safeezersize;
+
+import android.app.IntentService;
+import android.hardware.SensorEventListener;
+
+public class AccelListener extends IntentService implements SensorEventListener {
+}
